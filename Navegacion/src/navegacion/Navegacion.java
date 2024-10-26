12342179afa15d08628be1db660cf2b2308c6181
Jesus Huaripaucar <jesus.huaripaucar@vallegrande.edu.pe>
2024-10-26 04:43:05 -0500

    navegación entre ventanas

---
+++ Navegacion/src/navegacion/Navegacion.java
@@ -0,0 +1,15 @@
+package navegacion;
+
+
+class Navegacion {
+
+    
+    public static void main(String[] args) {
+
+        Ventana1 abrir = new Ventana1();
+        abrir.setVisible(true);
+                
+        
+    }
+    
+}
